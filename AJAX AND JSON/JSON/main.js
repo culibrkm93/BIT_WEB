a273cddd5f28701813700f843268a646425a7abc
BIT student <classroom@bgit.rs>
2019-05-29 18:00:38 +0200

    json

--- AJAX AND JSON/JSON/main.js
+++ AJAX AND JSON/JSON/main.js
@@ -1,18 +1,24 @@
-(function () {
+var btn = document.querySelector('.btn');
+var $body = document.querySelector('body');
+var $div = document.querySelector('.divic');
 
+
+
+function handler(e) {
     var imgRequest = new XMLHttpRequest();
     imgRequest.open('GET', 'https://dog.ceo/api/breeds/image/random');
     imgRequest.send();
-
     imgRequest.onload = function () {
         var data = JSON.parse(imgRequest.responseText);
-        console.log(data);
+        $div.innerHTML = '';
+
+        var imgURL = data.message;
+        var newImg = document.createElement('img');
+        newImg.setAttribute('src', imgURL);
+        $div.append(newImg);
     }
 
+}
 
 
-
-
-
-})();
-
+btn.addEventListener('click', handler);
